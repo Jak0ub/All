c498d6750ecc26702210eaa0ac1a65b68934a4bc
Jakoub <118178881+Jak0ub@users.noreply.github.com>
2024-09-05 23:08:12 +0200

    Update main.bat

--- batch/custom_taskkill/main.bat
+++ batch/custom_taskkill/main.bat
@@ -10,5 +10,5 @@ timeout /nobreak /t 1 >nul
 set /p oke=Chcete proces ukoncit?(1=Ano, ostatni=Ne): 
 if %oke% equ 1 (taskkill /IM %user% -F & taskkill /IM %user%.exe -F & cls)else (echo ukoncuji & timeout /nobreak /t 3 >nul & exit)
 tasklist | findstr /i %user% >nul
-if %errorlevel% equ 0 (echo proces byl uspesne ukoncen & timeout /nobreak /t 5 >nul & exit)else (echo proces nebylo mozne ukoncit & timeout /nobreak /t 5 >nul & exit)
+if %errorlevel% equ 0 (echo proces nebylo mozne ukoncit & timeout /nobreak /t 5 >nul & exit)else (echo proces byl uspesne ukoncen & timeout /nobreak /t 5 >nul & exit)
 
